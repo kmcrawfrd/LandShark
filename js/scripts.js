73a1191b579dca7c10941580caf6f712386d89dd
kmcrawfrd <km.crawford@uleth.ca>
2024-11-27 05:47:03 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$("#default-button").on("click", function() {
+    $("#shark").replaceWith('<img id="shark" src="imgs/sharky1.png">');
+});
+
+$("#skate-button").on("click", function() {
+    $("#shark").replaceWith('<img id="shark" src="imgs/sharky2.png">');
+});
+
+$("#fish-button").on("click", function() {
+    $("#shark").replaceWith('<img id="shark" src="imgs/sharky3.png">');
+});
+
+$("#guitar-button").on("click", function() {
+    $("#shark").replaceWith('<img id="shark" src="imgs/sharky4.png">');
+});
+
+$("#sleep-button").on("click", function() {
+    $("#shark").replaceWith('<img id="shark" src="imgs/sharky5.png">');
+});
